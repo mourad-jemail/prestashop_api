fd8e8302054eac647c1e32948e9e7d679d3299b8
Mourad JEMAIL <mourad.jemail@gmail.com>
2024-04-12 20:28:05 +0100

    Exported the package's API classes of the category feature.

--- lib/prestashop_api.dart
+++ lib/prestashop_api.dart
@@ -4,6 +4,8 @@ library prestashop_api;
 export 'package:dio/dio.dart';
 
 export 'src/base_config.dart';
+export 'src/category/model/category.dart';
+export 'src/category/network/category_enums.dart';
 export 'src/common/exceptions/custom_exceptions.dart';
 export 'src/common/exceptions/prestashop_error.dart';
 export 'src/common/model/id.dart';
@@ -14,3 +16,4 @@ export 'src/common/options/sort/sort.dart';
 export 'src/common/options/sort/sort_field_order.dart';
 export 'src/common/shared/utils.dart' show prettyPrint;
 export 'src/common/uri_builder.dart';
+export 'src/prestashop_api.dart';
