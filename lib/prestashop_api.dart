5739e9e90f9d94a47bd01039a7c47d3736734e72
Mourad JEMAIL <mourad.jemail@gmail.com>
2024-04-12 20:12:06 +0100

    Exported the package's API classes of common features.

---
+++ lib/prestashop_api.dart
@@ -0,0 +1,16 @@
+/// You can make APIs public here.
+library prestashop_api;
+
+export 'package:dio/dio.dart';
+
+export 'src/base_config.dart';
+export 'src/common/exceptions/custom_exceptions.dart';
+export 'src/common/exceptions/prestashop_error.dart';
+export 'src/common/model/id.dart';
+export 'src/common/model/received_entity.dart';
+export 'src/common/options/display/display.dart';
+export 'src/common/options/filter/filter.dart';
+export 'src/common/options/sort/sort.dart';
+export 'src/common/options/sort/sort_field_order.dart';
+export 'src/common/shared/utils.dart' show prettyPrint;
+export 'src/common/uri_builder.dart';
