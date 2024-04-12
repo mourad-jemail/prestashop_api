20d384ff608b972a560678ceab6a2b8ced057bf0
Mourad JEMAIL <mourad.jemail@gmail.com>
2024-04-12 21:51:15 +0100

    Created a PrestashopApi object.

--- example/main.dart
+++ example/main.dart
@@ -1,3 +1,12 @@
-void main() {
+import 'package:prestashop_api/prestashop_api.dart';
 
+void main() {
+  /// Create a PrestashopApi object
+  final prestashop = PrestashopApi(
+    BaseConfig(
+      baseUrl: 'www.your-website.com',
+      apiKey: 'YOUR-API-KEY-XXXXXXXXX',
+      protocol: Protocol.https,
+    ),
+  );
 }
