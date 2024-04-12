9fea0dacaf487f9b2fba6bfff66bc820ae182cd5
Mourad JEMAIL <mourad.jemail@gmail.com>
2024-04-12 20:06:52 +0100

    Added string constants.

---
+++ lib/src/common/shared/strings.dart
@@ -0,0 +1,7 @@
+///
+/// ERRORS
+///
+const String errorNoConnection =
+    "The connection errored: Failed Host lookup: 'www.example.com'";
+const String errorApi = 'Internal server error';
+const String errorTimeout = 'Timeout: No response from the server';
