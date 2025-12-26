2fde99633ecc743471305b7f71868fd656bac79c
MouradJEMAIL <mourad.jemail@gmail.com>
2025-12-26 01:15:42 +0100

    Added 'carriers' resource data source and updated URI builder with carriers path.

---
+++ lib/src/carriers/network/carrier_data_source.dart
@@ -0,0 +1,173 @@
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+
+import '../../base_config.dart';
+import '../../common/exceptions/custom_exceptions.dart';
+import '../../common/exceptions/prestashop_error.dart';
+import '../../common/extensions/dio_extensions.dart';
+import '../../common/model/remote_response.dart';
+import '../../common/options/display/display.dart';
+import '../../common/options/filter/filter.dart';
+import '../../common/options/sort/sort.dart';
+import '../../common/options/sort/sort_field_order.dart';
+import '../../common/shared/utils.dart';
+import '../../common/uri_builder.dart';
+import '../dto/carrier_output_dto.dart';
+import 'carrier_enums.dart';
+
+@LazySingleton()
+class CarrierDataSource {
+  final Dio _dio;
+  final BaseConfig _baseConfig;
+
+  CarrierDataSource(this._dio, this._baseConfig);
+
+  Future<RemoteResponse<CarrierOutputDTO>> getCarriers({
+    required int languageId,
+    Filter<CarrierFilterField>? filter,
+    Display<CarrierDisplayField>? display,
+    Sort<SortFieldOrder<CarrierSortField>>? sort,
+  }) async {
+    final uriBuilder = UriBuilder(
+      _baseConfig,
+      carriersPath,
+      languageId: languageId,
+    ).setFilter(filter).setDisplay(display).setSort(sort);
+
+    try {
+      Response response;
+      if (_dio.options.connectTimeout != null) {
+        response = await _dio
+            .getUri(
+              uriBuilder.uri,
+              options: Options(headers: buildHeaders(_baseConfig.apiKey)),
+            )
+            .timeout(
+              _dio.options.connectTimeout!,
+              onTimeout: () => throw NoServerResponseException(),
+            );
+      } else {
+        response = await _dio.getUri(
+          uriBuilder.uri,
+          options: Options(headers: buildHeaders(_baseConfig.apiKey)),
+        );
+      }
+
+      if (response.statusCode == 200) {
+        if (response.data.toString() == '[]') {
+          return const RemoteResponse<CarrierOutputDTO>(
+            CarrierOutputDTO(carrierDTOList: []),
+          );
+        }
+
+        final jsonMap = response.data as Map<String, dynamic>;
+
+        final carrierOutputDTO = CarrierOutputDTO.fromJson(jsonMap);
+
+        return RemoteResponse<CarrierOutputDTO>(carrierOutputDTO);
+      } else {
+        throw RestApiException(response.statusCode, response.statusMessage);
+      }
+    } on DioException catch (e) {
+      if (e.isConnectionError) {
+        throw HostLookupException(e.message);
+      } else if (e.response != null) {
+        if (e.response!.statusCode == 400 || e.response!.statusCode == 401) {
+          final (code, message) = buildPrestashopError(e.response?.data);
+
+          throw PrestashopError(e.response!.statusCode!, code, message);
+        } else {
+          throw RestApiException(
+            e.response?.statusCode,
+            e.response?.statusMessage,
+          );
+        }
+      } else {
+        rethrow;
+      }
+    }
+  }
+
+  Future<RemoteResponse<CarrierOutputDTO>> getCarriersPage({
+    required int languageId,
+    required int page,
+    required int perPage,
+    Filter<CarrierFilterField>? filter,
+    Display<CarrierDisplayField>? display,
+    Sort<SortFieldOrder<CarrierSortField>>? sort,
+  }) async {
+    final uriBuilder =
+        UriBuilder(_baseConfig, carriersPath, languageId: languageId)
+            .setLimit(page: page, perPage: perPage)
+            .setFilter(filter)
+            .setDisplay(display)
+            .setSort(sort);
+
+    try {
+      Response response;
+      if (_dio.options.connectTimeout != null) {
+        response = await _dio
+            .getUri(
+              uriBuilder.uri,
+              options: Options(headers: buildHeaders(_baseConfig.apiKey)),
+            )
+            .timeout(
+              _dio.options.connectTimeout!,
+              onTimeout: () => throw NoServerResponseException(),
+            );
+      } else {
+        response = await _dio.getUri(
+          uriBuilder.uri,
+          options: Options(headers: buildHeaders(_baseConfig.apiKey)),
+        );
+      }
+
+      if (response.statusCode == 200) {
+        if (response.data.toString() == '[]') {
+          return const RemoteResponse<CarrierOutputDTO>(
+            CarrierOutputDTO(carrierDTOList: []),
+            isNextPageAvailable: false,
+          );
+        }
+
+        final jsonMap = response.data as Map<String, dynamic>;
+
+        CarrierOutputDTO carrierOutputDTO = CarrierOutputDTO.fromJson(jsonMap);
+
+        final carrierDTOList = carrierOutputDTO.carrierDTOList;
+        bool? isNextPageAvailable = false;
+
+        if (carrierDTOList.length > perPage) {
+          carrierOutputDTO = carrierOutputDTO.copyWith(
+            carrierDTOList: List.from(carrierDTOList)..removeLast(),
+          );
+          isNextPageAvailable = true;
+        }
+
+        return RemoteResponse<CarrierOutputDTO>(
+          carrierOutputDTO,
+          isNextPageAvailable: isNextPageAvailable,
+        );
+      } else {
+        throw RestApiException(response.statusCode, response.statusMessage);
+      }
+    } on DioException catch (e) {
+      if (e.isConnectionError) {
+        throw HostLookupException(e.message);
+      } else if (e.response != null) {
+        if (e.response!.statusCode == 400 || e.response!.statusCode == 401) {
+          final (code, message) = buildPrestashopError(e.response?.data);
+
+          throw PrestashopError(e.response!.statusCode!, code, message);
+        } else {
+          throw RestApiException(
+            e.response?.statusCode,
+            e.response?.statusMessage,
+          );
+        }
+      } else {
+        rethrow;
+      }
+    }
+  }
+}
