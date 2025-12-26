06f7f84e82c2aafe5b0005051006d14e90d94212
MouradJEMAIL <mourad.jemail@gmail.com>
2025-12-26 03:57:10 +0100

    Updated `prestashop_api.dart` to export `carrier_enums.dart` instead of `carrier_data_source.dart`.

--- lib/prestashop_api.dart
+++ lib/prestashop_api.dart
@@ -9,7 +9,7 @@ export 'src/attachments/model/attachment.dart';
 export 'src/attachments/network/attachment_enums.dart';
 export 'src/base_config.dart';
 export 'src/carriers/model/carrier.dart';
-export 'src/carriers/network/carrier_data_source.dart';
+export 'src/carriers/network/carrier_enums.dart';
 export 'src/categories/model/category.dart';
 export 'src/categories/network/category_enums.dart';
 export 'src/common/exceptions/custom_exceptions.dart';
